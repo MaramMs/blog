0d7f848aa1449d29c3754b8fcfd15b51ea0218e2
Maran.I.Msabeh <maramismail2015@gmail.com>
2025-07-30 13:18:52 +0300

    delete change

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,3 @@
-// import Hero from "./components/Hero";
-// import hero from "../../public/assets/hero.jpg";
-// import { getPosts } from "../sanity/lib/getPosts";
-// import PostsWithClientWrapper from "./components/PostWithClientWrapper";
-// export default async function Home() {
-//   const posts = await getPosts();
-//   return (
-//     <div className="container mx-auto mt-[50px] md:mt-[96px]">
-
-//       <PostsWithClientWrapper posts={posts} />
-//     </div>
-//   );
-// }
-
-
 'use client'
 import { useEffect, useState } from 'react';
 import { getPosts } from '../studio/sanity/lib/getPosts';
